62fa14c710a2335e593e11d573ff8d31ab05ed7e
ivy <119665497+lvlvllvlvllvlvl@users.noreply.github.com>
2023-04-15 00:52:06 +0700

    fix test

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "App";
-
-test("renders title", () => {
-  render(<App />);
-  const titleElement = screen.getByText(/poetrage/i);
-  expect(titleElement).toBeInTheDocument();
-});
