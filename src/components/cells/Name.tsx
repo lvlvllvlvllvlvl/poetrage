f51c453d9a9c20f702b06ee20ad9b1d15cc6d323
ivy <119665497+lvlvllvlvllvlvl@users.noreply.github.com>
2023-09-29 11:36:53 +0700

    show level/qual in name column

--- src/components/cells/Name.tsx
+++ src/components/cells/Name.tsx
@@ -1,18 +1,16 @@
 import Link from "@mui/material/Link";
-import Tooltip from "@mui/material/Tooltip";
 import { GemIcons } from "components/GemIcons";
-import { Gem, getId, getQueryUrl } from "models/gems";
+import { Gem, getQueryUrl } from "models/gems";
 import { useAppSelector } from "state/store";
 
 export const Name = ({ gem }: { gem: Gem }) => {
   const league = useAppSelector((state) => state.app.league);
   return (
     <>
-      <Tooltip title={getId(gem)}>
-        <Link target="_blank" rel="noreferrer" href={getQueryUrl(gem, league?.name)}>
-          {gem.Name}
-        </Link>
-      </Tooltip>
+      <Link target="_blank" rel="noreferrer" href={getQueryUrl(gem, league?.name)}>
+        {gem.Level}/{gem.Quality}
+        {gem.Corrupted ? "c" : ""} {gem.Name}
+      </Link>
       <GemIcons gem={gem} />
     </>
   );
