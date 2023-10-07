cc0fc47553abc10e97dbb54a0502712af4fc3ba5
ivy <119665497+lvlvllvlvllvlvl@users.noreply.github.com>
2023-10-07 19:39:50 +0700

    add rate-limiting to poe api calls

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -1,41 +1,38 @@
 import axios from "axios";
 import { setupCache } from "axios-cache-adapter";
+import { AxiosRateLimiter } from "rate-limit-rules";
 import { forageStore } from "./localForage";
 
 const hour = 60 * 60 * 1000;
+
+const corsAdapter = (req: any) => {
+  if (req.url && !req.url.includes("github.io")) {
+    req.url = "https://corsproxy.io/?" + encodeURIComponent(req.url);
+  }
+  return axios.defaults.adapter!(req) as any;
+};
+
+const rateLimitAdapter = new AxiosRateLimiter({ adapter: corsAdapter }).request as any;
 
 export const cache = (async () => {
   return setupCache({
     store: await forageStore,
     maxAge: hour,
+    adapter: (req: any) =>
+      req.url?.includes("pathofexile.com") ? rateLimitAdapter(req) : corsAdapter(req),
     exclude: {
       query: false,
       methods: ["put", "patch", "delete"],
     },
-  });
+  } as any);
 })();
 
 export const api = axios.create({
   maxRedirects: 0,
-  adapter: async (req) => {
-    return await (await cache).adapter(req);
-  },
+  adapter: async (req) => await (await cache).adapter(req),
 });
 
 export const uncachedApi = axios.create({
   maxRedirects: 0,
-});
-
-api.interceptors.request.use((req) => {
-  if (req.url && !req.url.includes("github.io")) {
-    req.url = "https://corsproxy.io/?" + encodeURIComponent(req.url);
-  }
-  return req;
-});
-
-uncachedApi.interceptors.request.use((req) => {
-  if (req.url) {
-    req.url = "https://corsproxy.io/?" + encodeURIComponent(req.url);
-  }
-  return req;
+  adapter: rateLimitAdapter,
 });
